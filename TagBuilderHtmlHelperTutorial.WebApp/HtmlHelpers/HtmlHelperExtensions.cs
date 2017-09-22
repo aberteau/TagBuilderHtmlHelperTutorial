0997ee7f1b50c72c16281e6a6f17ee6ed0c30b4d
Amael BERTEAU <amael.berteau@tech-easy.fr>
2017-09-22 17:33:04 +0200

    Suppression ContentDisplayMode (Utilisation ToolTip)

--- TagBuilderHtmlHelperTutorial.WebApp/HtmlHelpers/HtmlHelperExtensions.cs
+++ TagBuilderHtmlHelperTutorial.WebApp/HtmlHelpers/HtmlHelperExtensions.cs
@@ -15,71 +15,65 @@ namespace Techeasy.TagBuilderHtmlHelperTutorial.WebApp.HtmlHelpers
             return helper.ButtonHtmlTag().SetTypeAttribute("submit").SetInnerText("Enregistrer");
         }
 
-        public static HtmlTag AnchorButtonHtmlTag(this HtmlHelper helper, string href, HtmlTag iconTag, String text, ContentDisplayMode contentDisplayMode)
+        public static HtmlTag AnchorButtonHtmlTag(this HtmlHelper helper, string href, HtmlTag iconTag, String tooltip)
         {
             HtmlTag tag = helper.AnchorButtonHtmlTag(href);
-            tag.SetInnerHtml(iconTag, text, contentDisplayMode);
+            tag.SetInnerHtml(iconTag).SetTooltip(tooltip);
             return tag;
         }
 
-        public static HtmlTag ActionButtonHtmlTag(this HtmlHelper helper, HtmlTag iconTag, String text, ContentDisplayMode contentDisplayMode)
+        public static HtmlTag ButtonHtmlTag(this HtmlHelper helper, HtmlTag iconTag, String tooltip)
         {
             HtmlTag tag = helper.ButtonHtmlTag();
-            tag.SetInnerHtml(iconTag, text, contentDisplayMode);
+            tag.SetInnerHtml(iconTag).SetTooltip(tooltip);
             return tag;
         }
 
-        public static HtmlTag ListActionAnchorButtonHtmlTag(this HtmlHelper helper, string href, string text)
+        public static HtmlTag ListActionAnchorButtonHtmlTag(this HtmlHelper helper, string href, string tooltip)
         {
             HtmlTag glyphiconSpanTag = helper.GlyphiconListSpanTag();
-            return helper.AnchorButtonHtmlTag(href, glyphiconSpanTag, text, ContentDisplayMode.IconAndText);
+            return helper.AnchorButtonHtmlTag(href, glyphiconSpanTag, tooltip);
         }
 
-        public static HtmlTag ModifyActionAnchorButtonHtmlTag(this HtmlHelper helper, string href, ContentDisplayMode contentDisplayMode = ContentDisplayMode.IconAndText)
+        public static HtmlTag ModifyActionAnchorButtonHtmlTag(this HtmlHelper helper, string href)
         {
             HtmlTag glyphiconSpanTag = helper.GlyphiconPencilSpanTag();
-            return helper.AnchorButtonHtmlTag(href, glyphiconSpanTag, "Modifier", contentDisplayMode);
+            return helper.AnchorButtonHtmlTag(href, glyphiconSpanTag, "Modifier");
         }
 
-        public static HtmlTag RemoveActionAnchorButtonHtmlTag(this HtmlHelper helper, string href, ContentDisplayMode contentDisplayMode)
+        public static HtmlTag RemoveActionAnchorButtonHtmlTag(this HtmlHelper helper, string href)
         {
             HtmlTag glyphiconSpanTag = helper.GlyphiconTrashSpanTag();
-            return helper.AnchorButtonHtmlTag(href, glyphiconSpanTag, "Supprimer", contentDisplayMode);
+            return helper.AnchorButtonHtmlTag(href, glyphiconSpanTag, "Supprimer");
         }
 
-        public static HtmlTag CancelActionAnchorButtonHtmlTag(this HtmlHelper helper, string href, ContentDisplayMode contentDisplayMode)
+        public static HtmlTag CancelActionAnchorButtonHtmlTag(this HtmlHelper helper, string href)
         {
             HtmlTag glyphiconSpanTag = helper.GlyphiconBanCircleSpanTag();
-            return helper.AnchorButtonHtmlTag(href, glyphiconSpanTag, "Annuler", contentDisplayMode);
+            return helper.AnchorButtonHtmlTag(href, glyphiconSpanTag, "Annuler");
         }
 
-        public static HtmlTag BackActionAnchorButtonHtmlTag(this HtmlHelper helper, string href, string text, ContentDisplayMode contentDisplayMode)
+        public static HtmlTag BackActionAnchorButtonHtmlTag(this HtmlHelper helper, string href, string text)
         {
             HtmlTag glyphiconSpanTag = helper.GlyphiconEjectSpanTag();
-            return helper.AnchorButtonHtmlTag(href, glyphiconSpanTag, text, contentDisplayMode);
+            return helper.AnchorButtonHtmlTag(href, glyphiconSpanTag, text);
         }
 
-        public static HtmlTag BackActionAnchorButtonHtmlTag(this HtmlHelper helper, string href, ContentDisplayMode contentDisplayMode)
+        public static HtmlTag BackActionAnchorButtonHtmlTag(this HtmlHelper helper, string href)
         {
-            return helper.BackActionAnchorButtonHtmlTag(href, "Retour", contentDisplayMode);
+            return helper.BackActionAnchorButtonHtmlTag(href, "Retour");
         }
 
-        public static HtmlTag SaveActionSubmitButtonHtmlTag(this HtmlHelper helper, ContentDisplayMode contentDisplayMode)
-        {
-            HtmlTag glyphiconSpanTag = helper.GlyphiconOkCircleSpanTag();
-            return helper.ActionButtonHtmlTag(glyphiconSpanTag, "Enregistrer", contentDisplayMode).SetTypeAttribute("submit");
-        }
-
-        public static HtmlTag AddActionSubmitButtonHtmlTag(this HtmlHelper helper, ContentDisplayMode contentDisplayMode)
+        public static HtmlTag AddActionSubmitButtonHtmlTag(this HtmlHelper helper)
         {
             HtmlTag glyphiconSpanTag = helper.GlyphiconPlusSpanTag();
-            return helper.ActionButtonHtmlTag(glyphiconSpanTag, "Ajouter", contentDisplayMode).SetTypeAttribute("submit");
+            return helper.ButtonHtmlTag(glyphiconSpanTag, "Ajouter").SetTypeAttribute("submit");
         }
 
-        public static HtmlTag AddActionAnchorButtonHtmlTag(this HtmlHelper helper, string href, ContentDisplayMode contentDisplayMode = ContentDisplayMode.IconAndText)
+        public static HtmlTag AddActionAnchorButtonHtmlTag(this HtmlHelper helper, string href)
         {
             HtmlTag glyphiconSpanTag = helper.GlyphiconPlusSpanTag();
-            return helper.AnchorButtonHtmlTag(href, glyphiconSpanTag, "Ajouter", contentDisplayMode);
+            return helper.AnchorButtonHtmlTag(href, glyphiconSpanTag, "Ajouter");
         }
     }
 }
