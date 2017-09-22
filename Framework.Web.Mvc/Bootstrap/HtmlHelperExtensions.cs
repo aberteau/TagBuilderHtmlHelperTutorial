6abe8233a2b8dfe9274bcc79e9ffb9c40c07a358
Amael BERTEAU <amael.berteau@tech-easy.fr>
2017-09-22 17:13:44 +0200

    Utilisation extension SpanTag dans HtmlHelperExtensions

--- Framework.Web.Mvc/Bootstrap/HtmlHelperExtensions.cs
+++ Framework.Web.Mvc/Bootstrap/HtmlHelperExtensions.cs
@@ -8,70 +8,70 @@ namespace Techeasy.Framework.Web.Mvc.Bootstrap
 
         public static HtmlTag GlyphiconListSpanTag(this HtmlHelper helper)
         {
-            HtmlTag tag = helper.HtmlTag("span");
+            HtmlTag tag = helper.SpanTag();
             tag.AddCssClass("glyphicon glyphicon-list");
             return tag;
         }
 
         public static HtmlTag GlyphiconFilmSpanTag(this HtmlHelper helper)
         {
-            HtmlTag tag = helper.HtmlTag("span");
+            HtmlTag tag = helper.SpanTag();
             tag.AddCssClass("glyphicon glyphicon-film");
             return tag;
         }
 
         public static HtmlTag GlyphiconPlusSpanTag(this HtmlHelper helper)
         {
-            HtmlTag tag = helper.HtmlTag("span");
+            HtmlTag tag = helper.SpanTag();
             tag.AddCssClass("glyphicon glyphicon-plus");
             return tag;
         }
 
         public static HtmlTag GlyphiconPencilSpanTag(this HtmlHelper helper)
         {
-            HtmlTag tag = helper.HtmlTag("span");
+            HtmlTag tag = helper.SpanTag();
             tag.AddCssClass("glyphicon glyphicon-pencil");
             return tag;
         }
 
         public static HtmlTag GlyphiconTrashSpanTag(this HtmlHelper helper)
         {
-            HtmlTag tag = helper.HtmlTag("span");
+            HtmlTag tag = helper.SpanTag();
             tag.AddCssClass("glyphicon glyphicon-trash");
             return tag;
         }
 
         public static HtmlTag GlyphiconFloppyDiskSpanTag(this HtmlHelper helper)
         {
-            HtmlTag tag = helper.HtmlTag("span");
+            HtmlTag tag = helper.SpanTag();
             tag.AddCssClass("glyphicon glyphicon-floppy-disk");
             return tag;
         }
 
         public static HtmlTag GlyphiconBanCircleSpanTag(this HtmlHelper helper)
         {
-            HtmlTag tag = helper.HtmlTag("span");
+            HtmlTag tag = helper.SpanTag();
             tag.AddCssClass("glyphicon glyphicon-ban-circle");
             return tag;
         }
 
         public static HtmlTag GlyphiconOkCircleSpanTag(this HtmlHelper helper)
         {
-            HtmlTag tag = helper.HtmlTag("span");
+            HtmlTag tag = helper.SpanTag();
             tag.AddCssClass("glyphicon glyphicon-ok-circle");
             return tag;
         }
 
         public static HtmlTag GlyphiconSearchSpanTag(this HtmlHelper helper)
         {
-            HtmlTag tag = helper.HtmlTag("span");
+            HtmlTag tag = helper.SpanTag();
             tag.AddCssClass("glyphicon glyphicon-search");
             return tag;
         }
 
         public static HtmlTag GlyphiconEjectSpanTag(this HtmlHelper helper)
         {
-            HtmlTag tag = helper.HtmlTag("span");
+            HtmlTag tag = helper.SpanTag();
             tag.AddCssClass("glyphicon glyphicon-eject");
             return tag;
         }
