4c1aa76d0ea1103794efb8d784a4d34a1d691c3b
Amael BERTEAU <amael.berteau@tech-easy.fr>
2017-09-22 17:22:18 +0200

    Ajout HtmlTagExtensions dans Framework.Web.Mvc.Bootstrap

---
+++ Framework.Web.Mvc/Bootstrap/HtmlTagExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Techeasy.Framework.Web.Mvc.Bootstrap
+{
+    public static class HtmlTagExtensions
+    {
+        public static HtmlTag SetTooltip(this HtmlTag htmlTag, string title)
+        {
+            HtmlTag tag = htmlTag.SetAttribute("data-toggle", "tooltip").SetAttribute("title", title);
+            return tag;
+        }
+    }
+}
